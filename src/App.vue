0a494286f3ac23f877027deed6ee70b4ee60cf2d
Arham Akmal <khanarham0029@gmail.com>
2024-06-26 13:54:57 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/Edusync.png" />
+  <SignUp />
+</template>
+
+<script>
+import SignUp from './components/SignUp.vue'
+export default {
+  name: 'App',
+  components: {
+    SignUp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Sign Up</h1>
+  <img alt="Edusync logo" src="./assets/Edusync.png" />
+
+  <SignUp />
+  <div class="signupRegister">
+    <input type="text" placeholder="Enter Name" />
+    <input type="email" placeholder="Enter Email" />
+    <input type="password" placeholder="Enter Password" />
+    <button>Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap');
+
+.alfa-slab-one-regular {
+  font-family: 'Alfa Slab One', serif;
+  font-weight: 400;
+  font-style: normal;
+}
+.signupRegister {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  margin: 0 auto;
+}
+.signupRegister input {
+  width: 300px;
+  height: 40px;
+}
+
+h1 {
+  font-family: 'Alfa Slab One', cursive;
+}
+</style>
